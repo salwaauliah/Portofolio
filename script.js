3362e975a68cc210f430cbb60dedd8f27c5f71f5
Salwa Aulia <165162074+salwaauliah@users.noreply.github.com>
2025-02-19 12:07:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-// Menu toggle state
 let isMenuOpen = false;
 
-// Toggle menu functionality
 const toggleMenu = () => {
     const navLinks = document.getElementById("nav-links");
     const menuIcon = document.getElementById("menu-icon");
@@ -16,7 +14,6 @@ const toggleMenu = () => {
     }
 };
 
-// Set active navigation link
 const setActiveLink = (id) => {
     document.querySelectorAll(".nav-items").forEach((link) => {
         link.classList.remove("active");
@@ -27,7 +24,6 @@ const setActiveLink = (id) => {
     }
 };
 
-// Intersection Observer for sections
 const observer = new IntersectionObserver(
     (entries) => {
         entries.forEach((entry) => {
@@ -39,88 +35,85 @@ const observer = new IntersectionObserver(
 );
 
 document.addEventListener("DOMContentLoaded", () => {
-            // Observe sections for active link highlighting
-            const sections = document.querySelectorAll("section");
-            sections.forEach((section) => observer.observe(section));
+    const sections = document.querySelectorAll("section");
+    sections.forEach((section) => observer.observe(section));
 
-            // Tab-related elements
-            const projectsTab = document.getElementById("projectsTab");
-            const certificatesTab = document.getElementById("certificatesTab");
-            const techStackTab = document.getElementById("techStackTab");
-            const contentContainer = document.getElementById("contentContainer");
+    const projectsTab = document.getElementById("projectsTab");
+    const certificatesTab = document.getElementById("certificatesTab");
+    const techStackTab = document.getElementById("techStackTab");
+    const contentContainer = document.getElementById("contentContainer");
 
-            // Validate if all required elements exist
-            if (!projectsTab || !certificatesTab || !techStackTab || !contentContainer) {
-                console.error("One or more tab elements or content container is missing.");
-                return;
-            }
+    if (!projectsTab || !certificatesTab || !techStackTab || !contentContainer) {
+        console.error("One or more tab elements or content container is missing.");
+        return;
+    }
 
-            // Project data
-            const projects = [{
-                    image: "./assets/project1.png",
-                    title: "DASHBOARD KELAS.ID",
-                    description: "Analyzes the learning activities of Kelas.id users, and offline user recommendations based on English specialization.",
-                },
-                {
-                    image: "./assets/project2.png",
-                    title: "DASHBOARD E-COMMERCE",
-                    description: "Analyzes e-commerce revenue. It shows total sales, monthly revenue trends, top 5 products with the highest revenue, and best-selling product categories.",
-                },
-                {
-                    image: "./assets/project3.png",
-                    title: "PROTOTYPE WEBSITE",
-                    description: "Design of the official e-commerce, which provides exclusive merchandise and albums for global fans.",
-                },
-                {
-                    image: "./assets/porto.png",
-                    title: "MY WEBSITE PORTFOLIO",
-                    description: "Showcasing the various projects I have worked on, including data analysis, visualization, and interactive dashboard development. Each project reflects my ability to process data to produce valuable insights, with a focus on accuracy, efficiency, and ease of interpretation.",
-                },
-            ];
+    // Project data
+    const projects = [{
+            image: "./assets/project1.png",
+            title: "DASHBOARD KELAS.ID",
+            description: "Analyzes the learning activities of Kelas.id users, and offline user recommendations based on English specialization.",
+        },
+        {
+            image: "./assets/project2.png",
+            title: "DASHBOARD E-COMMERCE",
+            description: "Analyzes e-commerce revenue. It shows total sales, monthly revenue trends, top 5 products with the highest revenue, and best-selling product categories.",
+        },
+        {
+            image: "./assets/project3.png",
+            title: "PROTOTYPE WEBSITE",
+            description: "Design of the official e-commerce, which provides exclusive merchandise and albums for global fans.",
+        },
+        {
+            image: "./assets/porto.png",
+            title: "MY WEBSITE PORTFOLIO",
+            description: "Showcasing the various projects I have worked on, including data analysis, visualization, and interactive dashboard development. Each project reflects my ability to process data to produce valuable insights, with a focus on accuracy, efficiency, and ease of interpretation.",
+        },
+    ];
 
-            // Certificates data
-            const certificates = [
-                "./assets/msib.jpg",
-                "./assets/karier.mu.jpg",
-                "./assets/karier.mu1.jpg",
-                "./assets/magang.jpg",
-                "./assets/himti.jpg",
-            ];
+    // Certificates data
+    const certificates = [
+        "./assets/msib.jpg",
+        "./assets/karier.mu.jpg",
+        "./assets/karier.mu1.jpg",
+        "./assets/magang.jpg",
+        "./assets/himti.jpg",
+    ];
 
-            // Tech stack data
-            const techStack = [
-                { name: "Tableau", logo: "./assets/tableau.jpg" },
-                { name: "Python", logo: "./assets/python.jpg" },
-                { name: "SQL", logo: "./assets/sql.jpg" },
-                { name: "HTML", logo: "./assets/html.jpg" },
-                { name: "Matlab", logo: "./assets/matlab.jpg" },
-                { name: "C++", logo: "./assets/c++.jpg" },
-            ];
+    // Tech stack data
+    const techStack = [
+        { name: "Tableau", logo: "./assets/tableau.jpg" },
+        { name: "Python", logo: "./assets/python.jpg" },
+        { name: "SQL", logo: "./assets/sql.jpg" },
+        { name: "HTML", logo: "./assets/html.jpg" },
+        { name: "Matlab", logo: "./assets/matlab.jpg" },
+        { name: "C++", logo: "./assets/c++.jpg" },
+    ];
 
-            // Render content based on tab
-            const renderContent = (tab) => {
-                contentContainer.innerHTML = "";
-                switch (tab) {
-                    case "Projects":
-                        renderProjects();
-                        break;
-                    case "Certificates":
-                        renderCertificates();
-                        break;
-                    case "Tech Stack":
-                        renderTechStack();
-                        break;
-                    default:
-                        contentContainer.innerHTML = "<p>No content available</p>";
-                        break;
-                }
-            };
+    // Render content based on tab
+    const renderContent = (tab) => {
+        contentContainer.innerHTML = "";
+        switch (tab) {
+            case "Projects":
+                renderProjects();
+                break;
+            case "Certificates":
+                renderCertificates();
+                break;
+            case "Tech Stack":
+                renderTechStack();
+                break;
+            default:
+                contentContainer.innerHTML = "<p>No content available</p>";
+                break;
+        }
+    };
 
-            const renderProjects = () => {
-                    projects.forEach((project) => {
-                                const projectCard = document.createElement("div");
-                                projectCard.classList.add("project");
-                                projectCard.innerHTML = `
+    const renderProjects = () => {
+        projects.forEach((project) => {
+            const projectCard = document.createElement("div");
+            projectCard.classList.add("project");
+            projectCard.innerHTML = `
                 <img src="${project.image}" alt="${project.title}" class="card-image">
                 <h2 class="project-title">${project.title}</h2>
                 <p class="project-description">${project.description}</p>
@@ -189,21 +182,21 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById("contact-form");
+    const form = document.getElementById("contact-form");
 
-  form.addEventListener("submit", function(e) {
-    e.preventDefault();
-    
-    const formData = {
-      name: document.getElementById("name").value,
-      email: document.getElementById("email").value,
-      message: document.getElementById("message").value,
-    };
+    form.addEventListener("submit", function(e) {
+        e.preventDefault();
 
-    // Display the form data (for demonstration)
-    console.log(formData);
+        const formData = {
+            name: document.getElementById("name").value,
+            email: document.getElementById("email").value,
+            message: document.getElementById("message").value,
+        };
 
-    // Show alert on form submission
-    alert("Form Submitted!");
-  });
+        // Display the form data (for demonstration)
+        console.log(formData);
+
+        // Show alert on form submission
+        alert("Form Submitted!");
+    });
 });
